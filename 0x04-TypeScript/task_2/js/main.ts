e02005d219768d17dbd2865aed845cc33a97790b
Elleni-Bisrat <ellenibisrat16@gmail.com>
2025-09-21 17:26:18 +0300

    update

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -66,10 +66,10 @@ function executeWork(employee: Director | Teacher): string {
 
 type Subjects = 'Math' | 'History';
 
-function teachClass(todayClass: Subjects): string {
+export function teachClass(todayClass: Subjects): string {
   if (todayClass === 'Math') {
     return 'Teaching Math';
-  } else if (todayClass === 'History') {
+  } else {
     return 'Teaching History';
   }
 }
