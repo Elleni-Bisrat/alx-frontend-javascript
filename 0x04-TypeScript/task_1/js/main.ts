fabcda07ebb0298c96278a26215b471bf68bf62e
Elleni Bisrat <ellenibisrat16@gmail.com>
2025-09-21 16:30:42 +0300

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -29,3 +29,10 @@ const director1: Director = {
 };
 
 console.log(director1);
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}.${lastName}`;
+};
+
